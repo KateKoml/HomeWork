d94bcf0a7e41405070d5ae3ad4424a9c409bbfcd
Kate Komleva <komlevakate99@gmail.com>
2022-09-14 19:37:49 +0300

    feat: add Task 1-2 Lesson4

---
+++ src/Lesson4/Task1.java
@@ -0,0 +1,11 @@
+package Lesson4;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n < 11) {
+            System.out.println(n);
+            n++;
+        }
+    }
+}
